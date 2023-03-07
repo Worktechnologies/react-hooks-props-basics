75ea4d4d2ddf0ce8674017bcd3b3560902c0256a
Parmjeet Kaur <work.technologies25@gmail.com>
2023-03-07 11:04:04 +1000

    d

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import BlogContent from "./BlogContent";
 import BlogPost from "./BlogPost";
 
 function App() {
   return <BlogPost />;
+  <BlogContent/>;
+  <Comment/>
 }
 
 export default App;
